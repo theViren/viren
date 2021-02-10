a29fa4e9599f8b512d0267c3f2707ddcf25616ef
Virendra Singh Solanki <72618659+theViren@users.noreply.github.com>
2021-02-10 18:34:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,37 +2,70 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:image" content="http://theviren.coolpage.biz/img/pro.jpg"/>
+<meta property="og:description" content="See My Protofolio"/>
 <link rel="icon" type="image/png" href="img/v-icon.png"/>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <title>Virendra Profile</title>
 <link rel="stylesheet" href="css/prostyle.css">
 </head>
 <body>
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="about.php">About Me</a>
+  <a href="contact.php">Contact Me</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>	
 
-<h1 style="text-align: center; color:white; background-color: tomato;">Welcome to My Profile</h1>
-<img class="image" src="img/pro.jpg" alt="Avatar" style="width:200px; "><br>
+</div>
+
+<div class="navbar" id="mynave" >
+<a><span style="cursor:pointer" onclick="openNav()">&#9776; </span></a>
+  <a href="index.php"><img src="img/v-icon.png" style="width:25px;height:25px;"></a>
+  <a href="index.php">Home</a>
+   <a href="resume.php">Resume</a>
+
+</div>	
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "50%";
+  document.getElementById("mynave").style.width = "0";
+  
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mynave").style.width = "100%";
+}
+</script>
+<br><br><br>
+<h1 style="font-size: 1.8em; text-align: center; color:white; background-color: tomato;">Welcome to My Profile</h1>
+<img id="image" src="img/pro.jpg" alt="Avatar" style="width:200px; "><br>
 <div class="card">
-  <div class="container">
+  <div class="container1" style="font-size: .8em;">
     <h3><b>Name&nbsp;<i class="fa fa-vine"></i>:</b>&nbsp;<span>Virendra Singh Solanki</span></h3> 
     <h3><b>Email&nbsp;<i class="fa fa-paper-plane"></i>:</b>&nbsp;<span>Viren.353.vss@gmail.com</span></h3>
     <h3><b>Address&nbsp;<i class="fa fa-address-card"></i>:</b>&nbsp;<span>Bhilwara</span></h3>
-    <h3><b>D.O.B&nbsp;<i class="fa fa-birthday-cake"></i>:</b>&nbsp;<span>10 May 1999</span></h3>   
+    <h3><b>D.O.B&nbsp;<i class="fa fa-birthday-cake"></i>:</b>&nbsp;<span>10 May 1999</span></h3> 
     
   </div>
+  
 </div><br><br>
 <div style="">
 	
-	<center><button class="info" onclick="window.open('https://sites.google.com/view/onlyinfo/About-Virendra-Singh-Solanki'); ">Know About Me</button></center><br>
-	<center><button class="info" onclick="window.open('https://www.linkedin.com/in/virendra-singh-solanki-9b6375187/');">Check My LinkedIn</button></center><br>
-	<center><button class="info" onclick="window.open('https://www.instagram.com/virendrapal_singh_21/');">Check My Instagram</button></center><br>
-	<center><button class="info" onclick="window.open('https://www.facebook.com/virendra.singhsolanki.127');">Check My Facebook</button></center><br>
-	<center><button class="info" onclick="window.open('https://wa.me/+918875573277');">Whatsapp Me</button></center><br>
-	<center><button class="info" onclick="window.open('https://mail.google.com/mail/?view=cm&fs=1&to=viren.353.vss@gmail.com');">Mail Me</button></center><br>
-	<center><button class="info" onclick="window.open('https://drive.google.com/file/d/1_bdurHlhThemFm3OFwFV8z-DeKgGXSD_/view?usp=sharing');">See My Resume</button></center><br>
-
-
+	<center><button class="info" onclick="window.open('https://sites.google.com/view/onlyinfo/About-Virendra-Singh-Solanki'); "><i style="float: left;" class="fa fa-info-circle"></i>&nbsp;Know About Me</button></center><br>
+	<center><button class="info" onclick="window.open('https://www.linkedin.com/in/virendra-singh-solanki-9b6375187/');"><i style="float: left;" class="fa fa-linkedin"></i>&nbsp;Check My LinkedIn</button></center><br>
+	<center><button class="info" onclick="window.open('https://www.instagram.com/thevirendrapal');"><i style="float: left;" class="fa fa-instagram"></i>&nbsp;Check My Instagram</button></center><br>
+	<center><button class="info" onclick="window.open('https://www.facebook.com/virendra.singhsolanki.127');"><i style="float: left;" class="fa fa-facebook"></i>&nbsp;Check My Facebook</button></center><br>
+	<center><button class="info" onclick="window.open('https://wa.me/+918875573277');"><i style="float: left;" class="fa fa-whatsapp"></i>&nbsp;Whatsapp Me</button></center><br>
+	<center><button class="info" onclick="window.open('https://mail.google.com/mail/?view=cm&fs=1&to=viren.353.vss@gmail.com');"><i style="float: left;" class="fa fa-envelope"></i>&nbsp;Mail Me</button></center><br>
+	<center><button class="info" onclick="window.open('https://drive.google.com/file/d/1_bdurHlhThemFm3OFwFV8z-DeKgGXSD_/view?usp=sharing');"><i style="float: left;" class="fa fa-eye"></i>&nbsp;See My Resume</button></center><br>
 
 	
 </div>
+<a href="whatsapp://send?text=&#128071See My Resume%20&#128071%20http://theviren.coolpage.biz/index.html" data-action="share/whatsapp/share" class="float" target="_blank"><i class="fa fa-whatsapp my-float"></i>
+</a>
+
 </body>
 </html>
