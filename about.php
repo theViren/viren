34b9669276fa12f311547b6d43e19ea1c3b3e898
Virendra Singh Solanki <72618659+theViren@users.noreply.github.com>
2021-02-10 19:17:26 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" type="image/png" href="https://user-images.githubusercontent.com/72618659/107515187-b345d580-6bd0-11eb-8ebc-ba4cb765a36d.png"/>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<title>Virendra Profile</title>
+<link rel="stylesheet" href="css/prostyle.css">
+</head>
+<body>
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="about.php">About Me</a>
+  <a href="contact.php">Contact Me</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>	
+
+</div>
+
+<div class="navbar" id="mynave" >
+<a><span style="cursor:pointer" onclick="openNav()">&#9776; </span></a>
+  <a href="index.php"><img src="https://user-images.githubusercontent.com/72618659/107515187-b345d580-6bd0-11eb-8ebc-ba4cb765a36d.png" style="width:25px;height:25px;"></a>
+  <a href="index.php">Home</a>
+   <a href="resume.php">Resume</a>
+
+</div>	
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "50%";
+  document.getElementById("mynave").style.width = "0";
+  
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mynave").style.width = "100%";
+}
+</script><br><br><br><br>
+<h3 style="text-align:center; background-color: tomato; color:white; padding: 10px;"> About Me</h3>
+<img src="img/aboutme.jpg" style="width:100%;padding:7px;border: 2px solid yellow;">
+<div style="padding:10px 10px 10px 10px; background-color: white; margin: 10px; font-size: 20px; ">
+<p>I was born on 10 May 1999 and his primary education was in Kelwa town where he lived for 3-years. After that, some of the rest of his studies took place in his uncle's place.<br> 
+
+Some years, his family shifted to Bhilwara where i studied up to class 8 at Happy Days Secondary School, Bhilwara. After this Virendra Singh Solanki went to Amet and completed his Secondary education from Tulsi Amrit Vidhyapeeth School. Where he made 87% in the 10th RBSE Board.<br>
+
+He studied her High School with Subhash Public Senior Secondary School, Kankroli. Where he made 84% in the 12th Rbse Board. After this, he did B.Tech from the Electronics branch due to an interest in engineering.</p>
+</div>
+</body>
+</html>
