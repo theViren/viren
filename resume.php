86e0175ced13347be99d57aa1d23fcd9fc490a5d
Virendra Singh Solanki <72618659+theViren@users.noreply.github.com>
2021-02-10 18:38:18 +0530

    Create resume.php

---
+++ resume.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:image" content="http://theviren.coolpage.biz/img/pro.jpg"/>
+<meta property="og:description" content="See My Protofolio"/>
+<link rel="icon" type="image/png" href="img/v-icon.png"/>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<title>Virendra Profile</title>
+<link rel="stylesheet" href="css/prostyle.css">
+</head>
+<body>
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="about.html">About Me</a>
+  <a href="contact.html">Contact Me</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>	
+
+</div>
+
+<div class="navbar" id="mynave" >
+<a><span style="cursor:pointer" onclick="openNav()">&#9776; </span></a>
+  <a href="index.html"><img src="img/v-icon.png" style="width:25px;height:25px;"></a>
+  <a href="index.html">Home</a>
+   <a href="resume.html">Resume</a>
+
+</div>	
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "70%";
+  document.getElementById("mynave").style.width = "0";
+  
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mynave").style.width = "100%";
+}
+</script><br><br>
+<h3 style="text-align:center; background-color: tomato; color:white; padding: 10px;">My Resume</h3>
+<img src="img/myresume.jpg" style="width:100%"><br>
+<a href="whatsapp://send?text=&#128071See My Resume%20&#128071%20http://theviren.coolpage.biz/resume.html" data-action="share/whatsapp/share" class="float" target="_blank"><i class="fa fa-whatsapp my-float"></i>
+</a>
+	</body>
+</html>
