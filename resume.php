2b58c68035fee151ba135555d9e444fdb07be3ff
Virendra Singh Solanki <72618659+theViren@users.noreply.github.com>
2021-02-13 14:34:34 +0530

    Update resume.php

--- resume.php
+++ resume.php
@@ -40,6 +40,7 @@ function closeNav() {
 </script><br><br><br>
 <h3 style="text-align:center; background-color: tomato; color:white; padding: 10px;">My Resume</h3>
 <img src="https://user-images.githubusercontent.com/72618659/107515173-afb24e80-6bd0-11eb-871f-892dbe50b7dc.jpg" style="width:100%"><br>
+	<center><button class="info" onclick="window.open('https://drive.google.com/uc?export=download&id=1_bdurHlhThemFm3OFwFV8z-DeKgGXSD_');"><i style="float: left;" class="fa fa-eye"></i>&nbsp;Download Resume</button></center><br>
 <a href="whatsapp://send?text=&#128071See My Resume%20&#128071%20https://theviren.herokuapp.com/resume.php" data-action="share/whatsapp/share" class="float" target="_blank"><i class="fa fa-whatsapp my-float"></i>
 </a>
 	</body>
